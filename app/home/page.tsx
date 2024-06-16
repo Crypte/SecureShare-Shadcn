599979f25f2a557382c664b5b12f6d9a1ccf9106
louisM <louisconnect@pm.me>
2024-06-16 23:36:16 +0200

    fix issues

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,21 +1,11 @@
 "use client";
 
-import { useState, useEffect } from 'react';
 import { Switch } from "@/components/Switch";
 
 export default function Page() {
-  const [_idObject, setIdObject] = useState(null);
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const _idObject = searchParams.get('idobject');
-    setIdObject(_idObject);
-    console.log("id object : " + _idObject);
-  }, []);
-
   return (
     <>
-      <Switch _idObject={_idObject}/>
+      <Switch />
     </>
   );
 }
